721532fb3d73e35c1fd27015d90ddb5fdac0f977
Handy-Surun Dugurzhap <sukhdug@gmail.com>
2018-03-22 14:05:55 +0700

    Home and help pages modified

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'users#index'
+   root 'static_pages#home'
   get '/hello', to: 'application#hello'
   get '/goodbye', to: 'application#goodbye'
 end
